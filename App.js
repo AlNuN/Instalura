78281341c634a53dec9fe21919ebfcf90e79caa7
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-02-02 09:51:12 -0300

    fetch data from API and like button

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // eslint-disable-next-line prettier/prettier
 import {
   FlatList,
 } from 'react-native';
+import lerFotos from './src/api/feed';
 import { Cabecalho } from './src/Components/Cabecalho';
 import { Foto } from './src/Components/Foto';
 
-const informacoes = [
-  { id: 1, usuario: 'Matheus' },
-  { id: 2, usuario: 'Luanne' },
-];
-
 const App = () => {
+  const [fotos, setFotos] = useState([]);
+
+  useEffect(() => {
+    lerFotos(setFotos);
+  }, []);
+
   return (
     <FlatList
-      data={informacoes}
+      data={fotos}
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item }) => (
         <>
-          <Cabecalho nomeUsuario={item.usuario} />
-          <Foto />
+          <Cabecalho
+            nomeUsuario={item.userName}
+            urlImage={item.userURL}
+          />
+          <Foto
+            urlFoto={item.url}
+            descricao={item.description}
+            qntLikes={item.likes}
+          />
         </>
       )}
     />
--- src/Components/Foto/Foto.js
+++ src/Components/Foto/Foto.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line prettier/prettier
 import {
   Image,
   Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
 import estilo from './estilo';
 
-const Foto = () => {
+const ImgLike = (curtiu) =>
+  curtiu
+    ? require('../../../res/img/s2-checked.png')
+    : require('../../../res/img/s2.png');
+
+const Foto = ({ urlFoto, descricao, qntLikes }) => {
+  const [curtiu, setCurtiu] = useState(false);
+  const [likes, setLikes] = useState(qntLikes);
+
+  const curtirFoto = () => {
+    let qnt = likes;
+    curtiu ? qnt-- : qnt++;
+    setLikes(qnt);
+    setCurtiu(!curtiu);
+  };
+
   return (
     <>
       <Image
-        source={require('../../../res/img/alura.jpg')}
+        source={{ uri: urlFoto }}
         style={estilo.imagem}
       />
-      <Text>Descrição da imagem</Text>
+      <Text>{descricao}</Text>
+      <View style={estilo.viewLike}>
+        <TouchableOpacity
+          onPress={curtirFoto}
+        >
+          <Image
+            source={ImgLike(curtiu)} 
+            style={estilo.like}
+          />
+        </TouchableOpacity>
+        <Text>curtidas {likes}</Text>
+      </View>
     </>
   );
 };
