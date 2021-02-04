3a5299839c87fadb6c7a556930965fccf22e0d80
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-02-04 10:22:03 -0300

    Small changes to run in my cellphone

--- src/api/feed.js
+++ src/api/feed.js
@@ -1,5 +1,11 @@
+import { Platform } from 'react-native';
+
 const lerFotos = async (callback) => {
-  const fotosHTTP = await fetch('http://10.0.2.2:3030/feed');
+  let ip = '10.0.2.2';
+  if (Platform.Version === 30) {
+    ip = '192.168.1.128';
+  }
+  const fotosHTTP = await fetch(`http://${ip}:3030/feed`);
   const fotosJson = await fotosHTTP.json();
   callback(fotosJson);
 };
